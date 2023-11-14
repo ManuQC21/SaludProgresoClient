db7ef5a50b8fe5e6bed65339df9a9d8674e2648b
ManuQC21 <mquispecueva1@upao.edu.pe>
2023-11-13 22:11:02 -0500

    validación del imprimir citas en aplazar.

--- app/src/main/java/com/upao/adapter/AplazarCitasAdapter.java
+++ app/src/main/java/com/upao/adapter/AplazarCitasAdapter.java
@@ -12,48 +12,67 @@ import com.squareup.picasso.Picasso;
 import com.upao.R;
 import com.upao.api.ConfigApi;
 import com.upao.entity.service.Citas;
+
 import java.util.List;
 
-public class AplazarCitasAdapter extends RecyclerView.Adapter<AplazarCitasAdapter.AplazarCitasViewHolder> {
+public class AplazarCitasAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
+    private static final int TIPO_NORMAL = 0;
+    private static final int TIPO_SIN_CITAS = 1;
     private List<Citas> citasList;
 
     public AplazarCitasAdapter(List<Citas> citasList) {
         this.citasList = citasList;
     }
 
+    @Override
+    public int getItemViewType(int position) {
+        if (citasList.isEmpty()) {
+            return TIPO_SIN_CITAS;
+        }
+        return TIPO_NORMAL;
+    }
+
     @NonNull
     @Override
-    public AplazarCitasViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_doctor_hora, parent, false);
-        return new AplazarCitasViewHolder(itemView);
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView;
+        if (viewType == TIPO_SIN_CITAS) {
+            itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_no_citas, parent, false);
+            return new SinCitasViewHolder(itemView);
+        } else {
+            itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_doctor_hora, parent, false);
+            return new AplazarCitasViewHolder(itemView);
+        }
     }
 
     @Override
-    public void onBindViewHolder(@NonNull AplazarCitasViewHolder holder, int position) {
-        Citas cita = citasList.get(position);
-        holder.textViewNombreDoctor.setText(cita.getMedico().getNombreMedico());
-        holder.textViewEspecialidadDoctor.setText(cita.getMedico().getEspecialidad());
-        holder.buttonHoraCita.setText(cita.getHoraCita().getHora());
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        if (getItemViewType(position) == TIPO_NORMAL) {
+            Citas cita = citasList.get(position);
+            AplazarCitasViewHolder viewHolder = (AplazarCitasViewHolder) holder;
+            viewHolder.textViewNombreDoctor.setText(cita.getMedico().getNombreMedico());
+            viewHolder.textViewEspecialidadDoctor.setText(cita.getMedico().getEspecialidad());
+            viewHolder.buttonHoraCita.setText(cita.getHoraCita().getHora());
 
-        // Utiliza Picasso para cargar imágenes desde una URL
-        // Asegúrate de tener la URL correcta para la imagen del doctor
-        String imageUrl = ConfigApi.baseUrlE + "/api/documento-almacenado/download/" + cita.getMedico().getFoto().getFileName();
-        Picasso.get().load(imageUrl).error(R.drawable.image_not_found).into(holder.imageViewDoctor);
+            String imageUrl = ConfigApi.baseUrlE + "/api/documento-almacenado/download/" + cita.getMedico().getFoto().getFileName();
+            Picasso.get().load(imageUrl).error(R.drawable.image_not_found).into(viewHolder.imageViewDoctor);
+        }
     }
 
     @Override
     public int getItemCount() {
-        return citasList.size();
+        return citasList.isEmpty() ? 1 : citasList.size();
     }
+
     public void setCitasList(List<Citas> citasList) {
         this.citasList = citasList;
         notifyDataSetChanged();
     }
+
     public static class AplazarCitasViewHolder extends RecyclerView.ViewHolder {
         ImageView imageViewDoctor;
-        TextView textViewNombreDoctor;
-        TextView textViewEspecialidadDoctor;
+        TextView textViewNombreDoctor, textViewEspecialidadDoctor;
         Button buttonHoraCita;
 
         public AplazarCitasViewHolder(@NonNull View itemView) {
@@ -62,6 +81,12 @@ public class AplazarCitasAdapter extends RecyclerView.Adapter<AplazarCitasAdapte
             textViewNombreDoctor = itemView.findViewById(R.id.textViewNombreDoctor);
             textViewEspecialidadDoctor = itemView.findViewById(R.id.textViewEspecialidadDoctor);
             buttonHoraCita = itemView.findViewById(R.id.buttonHoraCita);
+        }
+    }
+
+    public static class SinCitasViewHolder extends RecyclerView.ViewHolder {
+        public SinCitasViewHolder(@NonNull View itemView) {
+            super(itemView);
         }
     }
 }
